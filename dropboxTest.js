e657aa62bb334f840baf0366fc0185702dc62090
Jonas Keutel <jonas@keutel.de>
2017-11-03 14:38:34 +0100

    basic polling

--- dropboxTest.js
+++ dropboxTest.js
@@ -1,30 +1,38 @@
 var Dropbox = require('dropbox');
 var dbx = new Dropbox({ accessToken: 'R0OA10_0QoMAAAAAAAAQThs5uOU5UFcJjc0g9RlBGJwjvpD8PTdkCAwoOw-hGnvF' });
+const { exec } = require('child_process');
 
 const PATH = '/PhD/papers';
 const ENDING = '.pdf';
-const SEARCH = '.bibtex';
-const POLLING_INTERVAL = 1; // minute
+const POLLING_INTERVAL = parseFloat(process.argv[2]) || 1; // minute
 
+function poll() {
+    dbx.filesListFolder({path: PATH})
+      .then(function(response) {
+        // console.log(response);
+        var curr = Date.now();
+        for (entry of response.entries) {
+            if (entry.name.endsWith(ENDING) ) { // also check if timestamp works
+                var modified = new Date(entry.client_modified);
+                // console.log(modified);
+                var old = (curr - modified) / 1000 / 60; // in minutes
+                // console.log("File is ", old / 1000 / 60, " minutes old");
+                if (old < POLLING_INTERVAL) {
+                    console.log("NEW PDF!");
 
-dbx.filesListFolder({path: PATH})
-  .then(function(response) {
-    // console.log(response);
-    var curr = Date.now();
-    for (entry of response.entries) {
-        if (entry.name.endsWith(ENDING) ) { // also check if timestamp works
-            var modified = new Date(entry.client_modified);
-            // console.log(modified);
-            var old = (curr - modified) / 1000 / 60; // in minutes
-            // console.log("File is ", old / 1000 / 60, " minutes old");
-            if (old < POLLING_INTERVAL) {
-                console.log("NEW PDF!");
-            } else {
-                console.log("OLD PDF");
+                } else {
+                    console.log("OLD PDF");
+                    // exec("node test.js");
+                    exec('node test.js ' + entry.name, (error, stdout, stderr) => {
+                        console.log(stdout);
+                    });
+                }
             }
         }
-    }
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+}
+
+setInterval(poll, POLLING_INTERVAL * 1000 * 60);
